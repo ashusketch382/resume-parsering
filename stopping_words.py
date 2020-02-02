27e640921d89eb43ff3ef5c4f0d7fbbf50087ba2
ashutosh singh <ashutosh382singh@gmail.com>
2020-02-03 03:21:00 +0530

    modified

--- stopping_words.py
+++ stopping_words.py
@@ -29,7 +29,7 @@ stop_words = set(stopwords.words('english'))
 
 word_tokens = word_tokenize(example_sent) 
 
-filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+#filtered_sentence = [w for w in word_tokens if not w in stop_words] 
 
 filtered_sentence = [] 
 
